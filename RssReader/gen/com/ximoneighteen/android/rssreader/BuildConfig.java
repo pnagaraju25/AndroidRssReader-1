04ca3ec4fc743dfa252cd5b545560ed02a21b93a
xiei <ximon.eighteen@gmail.com>
2013-09-13 02:10:11 +0200

    More generated files to remove.

--- RssReader/gen/com/ximoneighteen/android/rssreader/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ximoneighteen.android.rssreader;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
